b527cad037f4b2f22e1162e7831311a151a6cecf
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-05-08 13:56:01 +0300

    compiling only with standart -std=c++17

--- src/s21_matrix.cc
+++ src/s21_matrix.cc
@@ -1,48 +1,64 @@
 #include <iostream>
 
 class S21Matrix {
-    private:
-        // Attributes
-        int rows_, cols_;         // Rows and columns
-        double **matrix_;         // Pointer to the memory where the matrix is allocated
+private:
+    // Attributes
+    int rows_, cols_;         // Rows and columns
+    double **matrix_;         // Pointer to the memory where the matrix is allocated
 
-    public:
-        S21Matrix() {}              // Default constructor
-        ~S21Matrix() {             // Destructor
-            delete[] matrix_;
-            rows_ = 0;
-            cols_ = 0;
-        }
 
-        void SumMatrix(const S21Matrix& other); 
-        // Other methods..
-        S21Matrix(int rows, int columns): rows_(rows), cols_(columns) {
-            double *pointer = new double[rows_ * cols_ *sizeof(double)];
-            double **matrix_local = new double*[rows_ * sizeof(double*)];
+    static int s_id_;         // статическая переменная 
+public:
+    S21Matrix() : rows_{0}, cols_{0}, matrix_{nullptr} {
 
-            for(int i = 0; i < rows; i++) {
-                *(matrix_local + i) = pointer + i*cols_;
+    }              // Default constructor
+    ~S21Matrix();               // Destructor
+
+    //int S21Matrix2; 
+    
+    void SumMatrix(const S21Matrix& other); 
+    // Other methods..
+    S21Matrix(int rows, int columns);
+
+    void Fill();
+
+    void Print() const {
+        for(int i = 0; i < rows_; i++) {
+            for(int j = 0; j < cols_; j++) {
+                std::cout<< matrix_[i][j] << ' ';
             }
-            matrix_ = matrix_local;
+            std::cout << '\n';
         }
+    }
 
-        void Fill() {
-            for(int i =0; i < rows_; i++) {
-                for(int j =0; j < cols_; j++) {
-                    matrix_[i][j] = i+j;
-                }
-            }
-        }
-
-        void Print() const {
-            for(int i =0; i < rows_; i++) {
-                for(int j =0; j < cols_; j++) {
-                    std::cout<< matrix_[i][j] << ' ';
-                }
-                std::cout << '\n';
-            }
-        }
+    bool EqMatrix(const S21Matrix& other);
 };
+
+S21Matrix::S21Matrix(int rows, int columns): rows_(rows), cols_(columns) {
+    double *pointer = new double[rows_ * cols_ *sizeof(double)];
+    double **matrix_local = new double*[rows_ * sizeof(double*)];
+
+        for(int i = 0; i < rows_; i++) {
+            *(matrix_local + i) = pointer + i*cols_;
+        }
+        matrix_ = matrix_local;
+}
+
+void S21Matrix::Fill() {
+    for(int i =0; i < rows_; i++) {
+        for(int j =0; j < cols_; j++) {
+            matrix_[i][j] = i+j;
+        }
+    }
+}
+
+//int S21Matrix::s_id_ = 8;
+
+S21Matrix::~S21Matrix() {             // Destructor
+        delete[] matrix_;
+        rows_ = 0;
+        cols_ = 0;
+}
 
 int main() {
     const int rows = 3;
@@ -52,6 +68,7 @@ int main() {
     mat.Fill();
     mat.Print();
 
+    S21Matrix mat2;
 
 
     return 0;
