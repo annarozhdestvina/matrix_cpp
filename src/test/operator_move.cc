e83cb21cfecc086dc938b9349d136e29051dc436
Anna Rozhdestvina <anna.rna225@gmail.com>
2023-07-10 19:44:08 +0300

    added test about operator = &&other

---
+++ src/test/operator_move.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "../s21_matrix.h"
+
+TEST(operator_move, test1) {
+    S21Matrix m1(3, 4);
+    m1.Fill();
+
+    S21Matrix m3 = m1; // cкопировали потому что потом он удалится 
+
+    S21Matrix m2 = (std::move(m1)); 
+
+    EXPECT_TRUE(m3.EqMatrix(m2));
+}
+
+TEST(operator_move, test2) {
+    S21Matrix m1(3, 4);
+    m1.Fill();
+    m1.MulNumber(-2);
+
+    S21Matrix m3 = m1; 
+
+    S21Matrix m2 = (std::move(m1)); 
+
+    EXPECT_TRUE(m3.EqMatrix(m2));
+}
